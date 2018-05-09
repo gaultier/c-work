2776d8fe935312333c751f2b90405dda5878f715
Philippe Gaultier <philippe.gaultier@ppro.com>
2018-05-10 01:07:08 +0200

    Add basic json parsing

--- airplane.c
+++ airplane.c
@@ -1,11 +1,65 @@
+#include <errno.h>
+#include <inttypes.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/param.h>
+#include "jsmn.h"
+#include "math.h"
 #include "utils.h"
 
-int main() {
+int main(int argc, const char* argv[]) {
+    if (argc != 2) return 0;
     char* content;
     uint64_t size;
-    readFile("airplane.json", &content, &size);
+    int r = readFile(argv[1], &content, &size);
+    if (r == -1) return 1;
+
     printf("Size: %llu\n", size);
+
+    jsmn_parser p;
+    jsmn_init(&p);
+    const uint32_t tok_count = 1000;
+    jsmntok_t* tok = calloc(sizeof(jsmntok_t), tok_count);
+
+    r = jsmn_parse(&p, content, size, tok, tok_count);
+    if (r < 0) {
+        fprintf(stderr, "Failed parsing: %d\n", r);
+        return 1;
+    }
+
+    /* Assume the top-level element is an object */
+    if (r < 1 || tok[0].type != JSMN_OBJECT) {
+        fprintf(stderr, "Object expected\n");
+        return 1;
+    }
+    printf("Found %d tokens\n", r);
+
+    for (int i = 1; i < r; i++) {
+        jsmntok_t token = tok[i];
+        printf("[%i]: type=%u, start=%u, end=%u, size=%u\n", i, token.type,
+               token.start, token.end, token.size);
+        if (token.type == JSMN_PRIMITIVE) {
+            char buf[100] = "";
+            char* start = &content[token.start];
+            uint64_t token_length = (uint64_t)(token.end - token.start + 1);
+            strlcpy(buf, start, MIN(token_length, 100));
+            double primitive = strtod(buf, NULL);
+            if (primitive == 0)
+                fprintf(stderr,
+                        "Could not convert primitive to double: `%s` is not a "
+                        "number\n",
+                        buf);
+            else if (primitive == HUGE_VAL || primitive == -HUGE_VAL)
+                fprintf(stderr,
+                        "Could not convert primitive to double, would "
+                        "overflow: %s errno=%d "
+                        "(input was `%s`)\n",
+                        strerror(errno), errno, buf);
+            else
+                printf("Primitive: %f\n", primitive);
+        }
+    }
+    return 0;
 }
--- utils.c
+++ utils.c
@@ -10,21 +10,23 @@
 int readFile(const char file_name[], char **str, uint64_t *size) {
     const int fd = open(file_name, O_RDONLY);
     if (fd == -1) {
-        fprintf(stderr, "Error opening file: %s", strerror(errno));
+        fprintf(stderr, "Error opening file: %s (%d)\n", strerror(errno),
+                errno);
         return -1;
     }
 
     struct stat s;
     const int res = fstat(fd, &s);
     if (res == -1) {
-        fprintf(stderr, "Error fstat : %s\n", strerror(errno));
+        fprintf(stderr, "Error fstat : %s (%d)\n", strerror(errno), errno);
         return -1;
     }
 
     *size = (uint64_t)s.st_size;
     *str = mmap(NULL, *size, PROT_READ, MAP_FILE | MAP_PRIVATE, fd, 0);
     if (*str == MAP_FAILED) {
-        fprintf(stderr, "Error mmapping file: %s\n", strerror(errno));
+        fprintf(stderr, "Error mmapping file: %s (%d)\n", strerror(errno),
+                errno);
         return -1;
     }
 
