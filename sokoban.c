e0203759e9c9fc540353a7652ceb7327740ad70f
Philippe Gaultier <philippe.gaultier@ppro.com>
2018-05-13 17:31:24 +0200

    Add map

--- sokoban.c
+++ sokoban.c
@@ -1,9 +1,10 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 enum Direction { DIR_UP, DIR_RIGHT, DIR_DOWN, DIR_LEFT };
-// enum Entity { NONE, WALL, MARIO, CRATE, CRATE_OK, OBJECTIVE };
+enum Entity { NONE, WALL, CRATE, CRATE_OK, OBJECTIVE, MARIO };
 
 int main() {
     IMG_Init(IMG_INIT_JPG);
@@ -48,39 +49,36 @@ int main() {
     SDL_FreeSurface(surface);
     SDL_Texture* current = mario[DIR_UP];
 
+    SDL_Texture* textures[5] = {NULL};
+
     SDL_Surface* crate_surface =
         IMG_Load("/Users/pgaultier/Downloads/sprites_mario_sokoban/crate.jpg");
-    SDL_Texture* crate = SDL_CreateTextureFromSurface(renderer, crate_surface);
+    textures[CRATE] = SDL_CreateTextureFromSurface(renderer, crate_surface);
     SDL_FreeSurface(crate_surface);
 
     SDL_Surface* crate_ok_surface = IMG_Load(
         "/Users/pgaultier/Downloads/sprites_mario_sokoban/crate_ok.jpg");
-    SDL_Texture* crate_ok =
+    textures[CRATE_OK] =
         SDL_CreateTextureFromSurface(renderer, crate_ok_surface);
     SDL_FreeSurface(crate_ok_surface);
 
     SDL_Surface* wall_surface =
         IMG_Load("/Users/pgaultier/Downloads/sprites_mario_sokoban/wall.jpg");
-    SDL_Texture* wall = SDL_CreateTextureFromSurface(renderer, wall_surface);
+    textures[WALL] = SDL_CreateTextureFromSurface(renderer, wall_surface);
     SDL_FreeSurface(wall_surface);
 
     SDL_Surface* objective_surface = IMG_Load(
         "/Users/pgaultier/Downloads/sprites_mario_sokoban/objective.png");
-    SDL_Texture* objective =
+    textures[OBJECTIVE] =
         SDL_CreateTextureFromSurface(renderer, objective_surface);
     SDL_FreeSurface(objective_surface);
 
+    FILE* map_file = fopen("map.txt", "r");
+    unsigned char map[12 * 12] = {0};
+    fread(map, 1, 12 * 12, map_file);
+    for (uint8_t i = 0; i < 12 * 12; i++) map[i] -= '0';
+
     SDL_Rect mario_rect = {.w = CELL_SIZE, .h = CELL_SIZE, .x = 0, .y = 0};
-    SDL_Rect crate_rects[1] = {
-        {.w = CELL_SIZE, .h = CELL_SIZE, .x = 3 * CELL_SIZE, .y = 0}};
-    SDL_Rect crate_ok_rects[1] = {
-        {.w = CELL_SIZE, .h = CELL_SIZE, .x = 4 * CELL_SIZE, .y = 0}};
-    SDL_Rect wall_rects[1] = {
-        {.w = CELL_SIZE, .h = CELL_SIZE, .x = 4 * CELL_SIZE, .y = CELL_SIZE}};
-    SDL_Rect objective_rects[1] = {{.w = CELL_SIZE,
-                                    .h = CELL_SIZE,
-                                    .x = 4 * CELL_SIZE,
-                                    .y = 2 * CELL_SIZE}};
     const uint16_t velocity = CELL_SIZE;
     bool running = true;
     while (running) {
@@ -115,22 +113,25 @@ int main() {
         }
         SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, current, NULL, &mario_rect);
-        for (uint8_t i = 0; i < 1; i++)
-            SDL_RenderCopy(renderer, crate, NULL, &crate_rects[i]);
-        for (uint8_t i = 0; i < 1; i++)
-            SDL_RenderCopy(renderer, crate_ok, NULL, &crate_ok_rects[1]);
-        for (uint8_t i = 0; i < 1; i++)
-            SDL_RenderCopy(renderer, wall, NULL, &wall_rects[1]);
-        for (uint8_t i = 0; i < 1; i++)
-            SDL_RenderCopy(renderer, objective, NULL, &objective_rects[1]);
-
+        for (uint8_t i = 0; i < 12 * 12; i++) {
+            if (map[i]) {
+                SDL_Rect rect = {.w = CELL_SIZE,
+                                 .h = CELL_SIZE,
+                                 .x = CELL_SIZE * i / 12,
+                                 .y = CELL_SIZE * (i % 12)};
+                SDL_RenderCopy(renderer, textures[map[i]], NULL, &rect);
+            }
+        }
         SDL_RenderPresent(renderer);
     }
-
     SDL_DestroyTexture(mario[0]);
     SDL_DestroyTexture(mario[1]);
     SDL_DestroyTexture(mario[2]);
     SDL_DestroyTexture(mario[3]);
+    SDL_DestroyTexture(textures[WALL]);
+    SDL_DestroyTexture(textures[CRATE]);
+    SDL_DestroyTexture(textures[CRATE_OK]);
+    SDL_DestroyTexture(textures[OBJECTIVE]);
     SDL_DestroyWindow(window);
     SDL_Quit();
     IMG_Quit();
