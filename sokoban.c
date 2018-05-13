621ae98250c19712055146ffc927883726a80cdd
Philippe Gaultier <philippe.gaultier@ppro.com>
2018-05-13 13:26:41 +0200

    Cap fps

--- sokoban.c
+++ sokoban.c
@@ -48,8 +48,11 @@ int main() {
     SDL_FreeSurface(surface);
     SDL_Texture* current = mario[DIR_UP];
 
+    const float time_frame = 1000 / 60;
     bool running = true;
+    uint64_t frame_count = 0;
     while (running) {
+        time_t time_start = time(NULL);
         SDL_Event e;
         SDL_PollEvent(&e);
         if (e.type == SDL_QUIT)
@@ -76,6 +79,11 @@ int main() {
         SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, current, NULL, NULL);
         SDL_RenderPresent(renderer);
+        frame_count++;
+        time_t time_end = time(NULL);
+        time_t time_diff = time_end - time_start;
+        if (time_diff < time_frame)
+            SDL_Delay((uint32_t)(time_frame - time_diff));
     }
 
     SDL_DestroyTexture(mario[0]);
