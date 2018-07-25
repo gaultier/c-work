222c10825a69dc4f681eb3caa1892efcba080df9
Philippe Gaultier <philippe.gaultier@ppro.com>
2018-07-25 18:20:38 +0200

    Tweak sudoku

--- sudoku.c
+++ sudoku.c
@@ -3,42 +3,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-bool is_row_valid(const uint8_t board[][9], uint8_t row);
-bool is_row_valid(const uint8_t board[][9], uint8_t row) {
+bool is_array_valid(const uint8_t array[9]);
+bool is_array_valid(const uint8_t array[9]) {
     for (uint8_t i = 1; i <= 9; i++) {
-        if (!memchr(&board[row], i, 9)) return false;
+        if (!memchr(array, i, 9)) return false;
     }
     return true;
+}
+
+bool is_row_valid(const uint8_t board[][9], uint8_t row);
+bool is_row_valid(const uint8_t board[][9], uint8_t row) {
+    return is_array_valid(board[row]);
 }
 
 bool is_column_valid(const uint8_t board[][9], uint8_t column);
 bool is_column_valid(const uint8_t board[][9], uint8_t column) {
-    bool found = false;
-    for (uint8_t i = 1; i <= 9; i++) {
-        for (uint8_t j = 0; j < 9; j++) {
-            if (board[j][column] == i) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) return false;
-    }
-    return true;
+    uint8_t array[9];
+    for (uint8_t j = 0; j < 9; j++) array[j] = board[j][column];
+
+    return is_array_valid(array);
 }
 
 bool is_square_valid(const uint8_t board[][9], uint8_t square);
 bool is_square_valid(const uint8_t board[][9], uint8_t square) {
-    bool found = false;
-    for (uint8_t i = 1; i <= 9; i++) {
-        for (uint8_t j = 0; j < 9; j++) {
-            if (board[j][square] == i) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) return false;
-    }
-    return true;
+    uint8_t array[9];
+    for (uint8_t i = 0; i < 3; i++)
+        for (uint8_t j = i; j < 3; j++) array[i * 3 + j] = board[i][j];
+
+    return is_array_valid(array);
 }
 
 int main() {
@@ -50,9 +42,9 @@ int main() {
         {3, 6, 4, 5, 6, 9, 8, 3, 7},
         {5, 5, 4, 5, 6, 9, 8, 3, 7},
         {4, 4, 4, 5, 6, 9, 8, 3, 7},
-        {8, 2, 4, 5, 6, 9, 8, 3, 7},
-        {7, 3, 4, 5, 6, 9, 8, 3, 7},
-        {6, 2, 4, 5, 6, 9, 8, 3, 7},
+        {8, 2, 4, 5, 6, 9, 1, 2, 3},
+        {7, 3, 4, 5, 6, 9, 6, 8, 7},
+        {6, 2, 4, 5, 6, 9, 5, 9, 4},
     };
     // clang-format on
     printf("%d\n", is_row_valid(board, 0));
