a1a7a44e1effb69e9ed52b1de883f26e7f5c0af3
Philippe Gaultier <philippe.gaultier@ppro.com>
2018-08-31 08:47:26 +0200

    --wip-- [skip ci]

--- sales_commission2.c
+++ sales_commission2.c
@@ -17,7 +17,6 @@ uint64_t split(char const* input, char*** names) {
     uint64_t size = 0;
     while ((token_str = strsep(&input_copy, " \n")) != NULL) {
         if (token_str[0] == '\0') continue;
-        printf("# `%s`\n", token_str);
         vec_add(*names, size, strdup(token_str));
     }
 
@@ -33,17 +32,14 @@ int main() {
     line_size = getline(&line, &ignore, stdin);  // Header `Revenue`
     line_size = getline(&line, &ignore, stdin);  // Empty line
     line_size = getline(&line, &ignore, stdin);  // Header list of names
-    /* input[input_size - 1] = '\0'; */
 
-    char** names = NULL;
-    uint64_t names_count = split(line, &names);
-    printf("Names #: %llu\n", names_count);
-    for (uint64_t i = 0; i < names_count; i++) printf("N: `%s`\n", names[i]);
+    char** people = NULL;
+    uint64_t people_count = split(line, &people);
 
+    char** revenues = NULL;
+    uint64_t revenues_count = 0;
     char** articles = NULL;
     uint64_t articles_count = 0;
-    char** expenses = NULL;
-    uint64_t expenses_count = 0;
     do {
         line_size = getline(&line, &ignore, stdin);
 
@@ -55,21 +51,16 @@ int main() {
 
         vec_add(articles, articles_count, words[0]);
         for (uint64_t w_i = 1; w_i < words_count; w_i++)
-            vec_add(expenses, expenses_count, words[w_i]);
+            vec_add(revenues, revenues_count, words[w_i]);
 
     } while (memmem(line, (uint64_t)line_size, "Expenses",
                     sizeof("Expenses") - 1) == NULL);
 
-    for (uint64_t a_i = 0; a_i < articles_count; a_i++)
-        printf("A: `%s`\n", articles[a_i]);
-    for (uint64_t e_i = 0; e_i < expenses_count; e_i++)
-        printf("E: `%s`\n", expenses[e_i]);
-
     line_size = getline(&line, &ignore, stdin);  // Empty line
-    line_size = getline(&line, &ignore, stdin);  // Header list of names
+    line_size = getline(&line, &ignore, stdin);  // Header list of people
 
-    char** revenues = NULL;
-    uint64_t revenues_count = 0;
+    char** expenses = NULL;
+    uint64_t expenses_count = 0;
     do {
         line_size = getline(&line, &ignore, stdin);
 
@@ -80,28 +71,23 @@ int main() {
         if (words_count <= 1) continue;
 
         for (uint64_t w_i = 1; w_i < words_count; w_i++)
-            vec_add(revenues, revenues_count, words[w_i]);
+            vec_add(expenses, expenses_count, words[w_i]);
 
     } while (line_size > 0);
-    for (uint64_t r_i = 0; r_i < revenues_count; r_i++)
-        printf("R: `%s`\n", revenues[r_i]);
 
-    /* for (uint64_t p = 0; p < people_count; p++) { */
-    /*     double commission_total = 0; */
-    /*     for (uint64_t a = 0; a < article_count; a++) { */
-    /*         char* revenue_str = get_cell_str(revenues, people_count, p,
-     * a);
-     */
-    /*         int64_t revenue = strtoll(revenue_str, NULL, 10); */
-    /*         char* expense_str = get_cell_str(expenses, people_count, p,
-     * a);
-     */
-    /*         int64_t expense = strtoll(expense_str, NULL, 10); */
-    /*         int64_t benefit = revenue - expense; */
-    /*         double commission = benefit * 0.062; */
-    /*         if (commission > 0) commission_total += commission; */
-    /*     } */
-    /*     printf("#Person %s: %.2f\n", tokens[1 + p].str,
-     * commission_total); */
-    /* } */
+    for (uint64_t p = 0; p < people_count; p++) {
+        double commission_total = 0;
+        for (uint64_t a = 0; a < articles_count; a++) {
+            char* revenue_str = revenues[people_count * a + p];
+            int64_t revenue = strtoll(revenue_str, NULL, 10);
+
+            char* expense_str = expenses[people_count * a + p];
+            int64_t expense = strtoll(expense_str, NULL, 10);
+
+            int64_t benefit = revenue - expense;
+            double commission = benefit * 0.062;
+            if (commission > 0) commission_total += commission;
+        }
+        printf("#Person %s: %.2f\n", people[p], commission_total);
+    }
 }
