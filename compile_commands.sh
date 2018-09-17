5088292833b8d07ef32b113aaac9925eaa94d3df
Philippe Gaultier <philippe.gaultier@ppro.com>
2018-09-17 18:44:09 +0200

    Generate compile_commands.json with script

---
+++ compile_commands.sh
@@ -0,0 +1,1 @@
+make -n | awk -v P="$(pwd)" 'BEGIN {print "["} /([A-Za-z_]+\.c)/ {  match($0, /[A-Za-z_]+\.c/); if (NR != 1) print ",";  print "{\"file\":\"" substr($0, RSTART, RLENGTH) "\",\"command\":\"" $0 "\", \"directory\":\"" P "\"}" } END {print "]"}' > compile_commands.json
