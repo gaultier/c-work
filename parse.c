00d0bdc3dabdc5ee348d2cc7bc6307f527aea474
Philippe Gaultier <philippe.gaultier@ppro.com>
2018-09-21 07:57:08 +0200

    --wip-- [skip ci]

--- parse.c
+++ parse.c
@@ -36,6 +36,19 @@ static bool match2(TokenType type1, TokenType type2, const Token** current,
     return false;
 }
 
+static bool match4(TokenType type1, TokenType type2, TokenType type3,
+                   TokenType type4, const Token** current, const Token* tokens,
+                   uint64_t tokens_count) {
+    if (check(*current, type1, tokens, tokens_count) ||
+        check(*current, type2, tokens, tokens_count) ||
+        check(*current, type3, tokens, tokens_count) ||
+        check(*current, type4, tokens, tokens_count)) {
+        advance(current, tokens, tokens_count);
+        return true;
+    }
+    return false;
+}
+
 static Expr* binary(Expr* left, const Token* operator, Expr* right) {
     Expr* expr = malloc(sizeof(Expr));
     *expr = (Expr){.left = left, .op = operator, .right = right };
@@ -43,23 +56,80 @@ static Expr* binary(Expr* left, const Token* operator, Expr* right) {
     return expr;
 }
 
-static Expr* comparison() {
-    Expr* expr = malloc(sizeof(Expr));
+static Expr* primary(const Token** current, const Token* tokens,
+                     uint64_t tokens_count) {
+    return NULL;  // TODO
+}
+
+static Expr* expr_unary(Expr* op, Expr* right, const Token** current,
+                        const Token* tokens, uint64_t tokens_count) {
+    return NULL;  // TODO
+}
+
+static Expr* unary(const Token** current, const Token* tokens,
+                   uint64_t tokens_count) {
+    if (match2(TokenTypeBang, TokenTypeMinus, current, tokens, tokens_count)) {
+        const Token* op = *(current - 1);
+        Expr* right = unary(current, tokens, tokens_count);
+        return expr_unary(op, right, current, tokens, tokens_count);
+    }
+    return primary(current, tokens, tokens_count);
+}
+
+static Expr* multiplication(const Token** current, const Token* tokens,
+                            uint64_t tokens_count) {
+    Expr* expr = unary(current, tokens, tokens_count);
+
+    while (
+        match2(TokenTypeSlash, TokenTypeStar, current, tokens, tokens_count)) {
+        const Token* op = *(current - 1);
+        Expr* right = unary(current, tokens, tokens_count);
+        expr = binary(expr, op, right);
+    }
+
+    return expr;
+}
+
+static Expr* addition(const Token** current, const Token* tokens,
+                      uint64_t tokens_count) {
+    Expr* expr = multiplication(current, tokens, tokens_count);
+
+    while (match4(TokenTypeGreater, TokenTypeGreaterEqual, TokenTypeLess,
+                  TokenTypeLessEqual, current, tokens, tokens_count)) {
+        const Token* op = *(current - 1);
+        Expr* right = multiplication(current, tokens, tokens_count);
+        expr = binary(expr, op, right);
+    }
+
+    return expr;
+}
+
+static Expr* comparison(const Token** current, const Token* tokens,
+                        uint64_t tokens_count) {
+    Expr* expr = addition(current, tokens, tokens_count);
+
+    while (match4(TokenTypeGreater, TokenTypeGreaterEqual, TokenTypeLess,
+                  TokenTypeLessEqual, current, tokens, tokens_count)) {
+        const Token* op = *(current - 1);
+        Expr* right = addition(current, tokens, tokens_count);
+        expr = binary(expr, op, right);
+    }
+
     return expr;
 }
 
 static Expr* equality(const Token** current, const Token* tokens,
                       uint64_t tokens_count) {
-    Expr* left = comparison();
+    Expr* expr = comparison(current, tokens, tokens_count);
 
     while (match2(TokenTypeBangEqual, TokenTypeEqualEqual, current, tokens,
                   tokens_count)) {
         const Token* op = *(current - 1);
-        Expr* right = comparison();
-        left = binary(left, op, right);
+        Expr* right = comparison(current, tokens, tokens_count);
+        expr = binary(expr, op, right);
     }
 
-    return left;
+    return expr;
 }
 
 static Expr* expression(const Token** current, const Token* tokens,
