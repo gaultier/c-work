340006502aa5071e5d0bdd57a4139fc88b80e769
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-01-06 15:42:37 +0100

    Add times

--- fec.c
+++ fec.c
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <sys/time.h>
 #include <unistd.h>
 
 int readFile(const char file_name[], char **str, uint64_t *size) {
@@ -32,45 +33,72 @@ int readFile(const char file_name[], char **str, uint64_t *size) {
 
 KHASH_MAP_INIT_STR(str, uint64_t)
 
-int main(int argc, char *argv[]) {
+int main() {
     char *file = NULL;
     uint64_t file_size = 0;
     if (readFile("fec.dataset.txt", &file, &file_size) != 0) return errno;
 
     // #1: Total number of lines
-    if (strcmp(argv[1], "1") == 0) {
+    {
+        struct timeval start;
+        gettimeofday(&start, NULL);
         uint64_t line = 0;
         for (uint64_t i = 0; i < file_size; i++) line += (file[i] == '\n');
         printf("Line count: %llu\n", line);
+
+        struct timeval end;
+        gettimeofday(&end, NULL);
+        printf("Time #1: %ld ms\n", (end.tv_sec - start.tv_sec) * 1000 +
+                                        (end.tv_usec - start.tv_usec) / 1000);
     }
 
     // #2 Print out the 432nd and 43243rd names (8th field).
-    if (strcmp(argv[1], "2") == 0) {
-        uint64_t line = 1;
+    {
+        struct timeval start;
+        gettimeofday(&start, NULL);
+
+        uint64_t line_count = 0;
+        char *line = alloca(1024);
+        char *orig_line = line;
         char *f = file;
-        while (line <= 43243) {
-            // If we are at the start of the interesting lines
-            if ((line == 432 || line == 43243) && *(f - 1) == '\n') {
-                // Then advance to the 7th separator i.e the beginning of the
-                // 8th field i.e name
-                for (uint64_t k = 1; k <= 7; k++)
-                    while (*++f != '|')
-                        ;
 
-                f++;  // Swallow the trailing '|'
+        while (f < file + file_size) {
+            if (f == file || *(f - 1) == '\n') {  // Start of line
+                line_count++;
+                if (line_count > 43243) break;
 
-                // Print the name and advance
-                while (*f != '|') putchar(*f++);
-                putchar('\n');
-            }
+                char *next_eol = strchr(f, '\n');
+                line = orig_line;
+                memcpy(line, f, (next_eol - f));
 
-            if (*f == '\n') line++;
-            f++;
+                char *token;
+                uint8_t token_count = 1;
+                char full_name[100];
+                while ((token = strsep(&line, "|")) != NULL) {
+                    if (token_count == 8) {
+                        strncpy(full_name, token, 99);
+                        full_name[99] = '\0';
+                        break;
+                    }
+                    token_count++;
+                }
+                if (line_count == 432 || line_count == 43243)
+                    printf("Line %llu: %s\n", line_count, full_name);
+
+                f = next_eol;
+            } else
+                f++;
         }
+        struct timeval end;
+        gettimeofday(&end, NULL);
+        printf("Time #2: %ld ms\n", (end.tv_sec - start.tv_sec) * 1000 +
+                                        (end.tv_usec - start.tv_usec) / 1000);
     }
 
     // # 3: Count how many donations occurred in each month (5th field)
-    if (strcmp(argv[1], "3") == 0) {
+    {
+        struct timeval start;
+        gettimeofday(&start, NULL);
         khiter_t k;
         khash_t(str) *monthly_count = kh_init(str);
 
@@ -111,10 +139,16 @@ int main(int argc, char *argv[]) {
         uint64_t count;
         kh_foreach(monthly_count, month, count,
                    printf("%s: %llu\n", month, count));
+        struct timeval end;
+        gettimeofday(&end, NULL);
+        printf("Time #3: %ld ms\n", (end.tv_sec - start.tv_sec) * 1000 +
+                                        (end.tv_usec - start.tv_usec) / 1000);
     }
 
     // # 4: Most common first name in the data and how many times it occurs
-    if (strcmp(argv[1], "4") == 0) {
+    {
+        struct timeval start;
+        gettimeofday(&start, NULL);
         khiter_t k;
         khash_t(str) *names = kh_init(str);
 
@@ -160,5 +194,9 @@ int main(int argc, char *argv[]) {
         });
 
         printf("%s: %llu\n", most_common_full_name, max_count);
+        struct timeval end;
+        gettimeofday(&end, NULL);
+        printf("Time #4: %ld ms\n", (end.tv_sec - start.tv_sec) * 1000 +
+                                        (end.tv_usec - start.tv_usec) / 1000);
     }
 }
