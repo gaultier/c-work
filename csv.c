226b6533ac60c36187a1e59cefbe47d4450b8d55
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-01-31 16:28:47 +0100

    --wip-- [skip ci]

--- csv.c
+++ csv.c
@@ -10,52 +10,54 @@
 #include "sds.c"
 #include "sds.h"
 
-static int sumWithFscanf(char sep[], size_t target_line, size_t target_column,
-                         char file_name[]) {
-    FILE* file = fopen(file_name, "r");
-    if (!file) {
-        fprintf(stderr, "The file %s could not be opened: %d %s\n", file_name,
-                errno, strerror(errno));
-        return errno;
-    }
-    char* line = NULL;
-    int token_count = 0;
-    size_t line_no = 0;
-    size_t line_cap = 0;
+// static int sumWithFscanf(char sep[], size_t target_line, size_t
+// target_column,
+//                         char file_name[]) {
+//    FILE* file = fopen(file_name, "r");
+//    if (!file) {
+//        fprintf(stderr, "The file %s could not be opened: %d %s\n", file_name,
+//                errno, strerror(errno));
+//        return errno;
+//    }
+//    char* line = NULL;
+//    int token_count = 0;
+//    size_t line_no = 0;
+//    size_t line_cap = 0;
+//
+//    int res = 0;
+//    while (line_no < target_line && (res = getline(&line, &line_cap, file)) >
+//    0)
+//        line_no++;
+//
+//    if (res == -1 && errno != 0) {
+//        fprintf(stderr,
+//                "An error occurred while reading the file %s: %d %s %d\n",
+//                file_name, errno, strerror(errno), res);
+//        return errno;
+//    }
+//
+//    if (line_no < target_line) {
+//        fprintf(stderr, "The file only contains %zu lines\n", line_no);
+//        return ERANGE;
+//    }
+//
+//    sds* tokens = sdssplitlen(line, res, sep, strlen(sep), &token_count);
+//    if (token_count < target_column) {
+//        fprintf(stderr,
+//                "The line %zu is malformed: does not contain at least %zu "
+//                "columns\n",
+//                line_no, target_column);
+//        fprintf(stderr, "Line: %s", line);
+//        return ERANGE;
+//    }
+//
+//    const char* const token = tokens[target_column - 1];
+//    printf("%zu:%zu: %s\n", target_line, target_column, token);
+//
+//    return 0;
+//}
 
-    int res = 0;
-    while (line_no < target_line && (res = getline(&line, &line_cap, file)) > 0)
-        line_no++;
-
-    if (res == -1 && errno != 0) {
-        fprintf(stderr,
-                "An error occurred while reading the file %s: %d %s %d\n",
-                file_name, errno, strerror(errno), res);
-        return errno;
-    }
-
-    if (line_no < target_line) {
-        fprintf(stderr, "The file only contains %zu lines\n", line_no);
-        return ERANGE;
-    }
-
-    sds* tokens = sdssplitlen(line, res, sep, strlen(sep), &token_count);
-    if (token_count < target_column) {
-        fprintf(stderr,
-                "The line %zu is malformed: does not contain at least %zu "
-                "columns\n",
-                line_no, target_column);
-        fprintf(stderr, "Line: %s", line);
-        return ERANGE;
-    }
-
-    const char* const token = tokens[target_column - 1];
-    printf("%zu:%zu: %s\n", target_line, target_column, token);
-
-    return 0;
-}
-
-static int readFile(const char file_name[], char** str, uint64_t* size) {
+static int read_file(const char file_name[], char** str, uint64_t* size) {
     const int fd = open(file_name, O_RDONLY);
     if (fd == -1) return errno;
 
@@ -76,32 +78,9 @@ static int readFile(const char file_name[], char** str, uint64_t* size) {
     return 0;
 }
 
-static int sumWithMmap(char sep[], size_t target_line, size_t target_column,
-                       char file_name[]) {
-    char* file = NULL;
-    uint64_t file_size = 0;
-    if (readFile(file_name, &file, &file_size) != 0) return errno;
-
-    const char* cur = file;
-    size_t line_no = 1;
-
-    while (cur < file + file_size && line_no < target_line) {
-        cur = strchr(cur, '\n');
-        if (cur == file + file_size) {
-            fprintf(stderr, "The file only contains %zu lines\n", line_no);
-            return ERANGE;
-        }
-        cur++;  // Swallow the newline
-        line_no++;
-    }
-
-    if (line_no < target_line) {
-        fprintf(stderr, "The file only contains %zu lines\n", line_no);
-        return ERANGE;
-    }
-
-    const char* const eol = strchr(cur, '\n');
-    sds line = sdsnewlen(cur, eol - cur);
+static int line_print_cell(sds line, size_t line_no, const char sep[],
+                           size_t sep_len, size_t target_line,
+                           size_t target_column) {
     int token_count = 0;
     sds* tokens =
         sdssplitlen(line, sdslen(line), sep, strlen(sep), &token_count);
@@ -115,20 +94,80 @@ static int sumWithMmap(char sep[], size_t target_line, size_t target_column,
 
     const char* const token = tokens[target_column - 1];
     printf("%zu:%zu: %s\n", target_line, target_column, token);
+    return 0;
+}
+
+static int line_count_seps(const char* line, size_t line_no, const char sep[],
+                           size_t sep_len) {
+    size_t count = 0;
+    const char* sep_it = line;
+    while ((sep_it = strstr(sep_it, sep))) count++, sep_it += sep_len;
+
+    printf("%zu: %zu\n", line_no, count);
+    return count;
+}
+
+static int count_seps(const char file_name[], const char sep[]) {
+    char* file = NULL;
+    uint64_t file_size = 0;
+    if (read_file(file_name, &file, &file_size) != 0) return errno;
+
+    const char* cur = file;
+    size_t line_no = 0;
+
+    while (cur < file + file_size) {
+        const char* const eol = strchr(cur, '\n');
+        size_t line_len = eol - cur;
+        char* line = malloc(line_len + 1);
+        memcpy(line, cur, line_len);
+        line[line_len] = '\0';
+        line_count_seps(line, line_no, sep, strlen(sep));
+        free(line);
+
+        cur = strchr(cur, '\n') + 1;
+        line_no++;
+    }
 
     return 0;
 }
 
+static int print_cell(const char file_name[], const char sep[],
+                      size_t target_line, size_t target_column) {
+    char* file = NULL;
+    uint64_t file_size = 0;
+    if (read_file(file_name, &file, &file_size) != 0) return errno;
+
+    const char* cur = file;
+    size_t line_no = 1;
+
+    while (cur < file + file_size && line_no < target_line) {
+        cur = strchr(cur, '\n') + 1;
+        line_no++;
+    }
+
+    if (line_no < target_line) {
+        fprintf(stderr, "The file only contains %zu lines\n", line_no);
+        return ERANGE;
+    }
+
+    const char* const eol = strchr(cur, '\n');
+    sds line = sdsnewlen(cur, eol - cur);
+    return line_print_cell(line, line_no, sep, strlen(sep), target_line,
+                           target_column);
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 6) {
-        printf("Usage: %s impl separator line column file\n", argv[0]);
+        printf("Usage: %s file separator cmd line column\n", argv[0]);
         return 1;
     }
 
-    if (strcmp(argv[1], "scanf") == 0)
-        return sumWithFscanf(argv[2], strtoull(argv[3], NULL, 10),
-                             strtoull(argv[4], NULL, 10), argv[5]);
-    else
-        return sumWithMmap(argv[2], strtoull(argv[3], NULL, 10),
-                           strtoull(argv[4], NULL, 10), argv[5]);
+    const char* file = argv[1];
+    const char* sep = argv[2];
+    const char* cmd = argv[3];
+    if (strcmp(cmd, "print_cell") == 0)
+        return print_cell(file, sep, strtoull(argv[4], NULL, 10),
+                          strtoull(argv[5], NULL, 10));
+    else if (strcmp(cmd, "count_seps") == 0)
+        return count_seps(file, sep);
 }
---
+++ strstr.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    const char* text = "the brown fox jumps over the lazy dog";
+    char* s = strstr(text, " ");
+    s = strstr(text, " ");
+}
