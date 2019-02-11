55f0d348e9652247cc44b5b35a7e2f0c0226bab0
Philippe Gaultier <philippe.gaultier@ppro.com>
2019-02-11 12:03:16 +0100

    Improve jemalloc example

--- jmalloc.c
+++ jmalloc.c
@@ -1,17 +1,78 @@
 // cc % -ljemalloc && ./a.out 100
-// Prints the number of allocated bytes with jemalloc
+// Prints stats with jemalloc
+#include <errno.h>
 #include <jemalloc/jemalloc.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
     size_t count = strtoll(argv[1], NULL, 10);
 
-    size_t* numbers = calloc(1, count);
+    size_t* numbers = malloc(count * sizeof(size_t));
+    if (numbers == NULL) {
+        fprintf(stderr, "Could not allocate: %d %s\n", errno, strerror(errno));
+        exit(errno);
+    }
+    for (size_t i = 0; i < count; i++) numbers[i] = rand();
+    printf("%zu\n", numbers[count - 1]);
 
-    size_t allocated = 0;
-    size_t allocated_len = sizeof(allocated);
-    mallctl("stats.allocated", &allocated, &allocated_len, NULL, 0);
+    {
+        size_t allocated = 0;
+        size_t allocated_len = sizeof(allocated);
+        mallctl("stats.allocated", &allocated, &allocated_len, NULL, 0);
+        printf("allocated: %zu\n", allocated);
 
-    printf("Allocated: %zu\n", allocated);
+        size_t active = 0;
+        size_t active_len = sizeof(active);
+        mallctl("stats.active", &active, &active_len, NULL, 0);
+        printf("active: %zu\n", active);
+
+        size_t mapped = 0;
+        size_t mapped_len = sizeof(mapped);
+        mallctl("stats.mapped", &mapped, &mapped_len, NULL, 0);
+        printf("mapped: %zu\n", mapped);
+
+        size_t retained = 0;
+        size_t retained_len = sizeof(retained);
+        mallctl("stats.retained", &retained, &retained_len, NULL, 0);
+        printf("retained: %zu\n", retained);
+
+        size_t resident = 0;
+        size_t resident_len = sizeof(resident);
+        mallctl("stats.resident", &resident, &resident_len, NULL, 0);
+        printf("resident: %zu\n", resident);
+    }
+
+    size_t new_count = count * 2;
+    numbers = realloc(numbers, new_count * sizeof(size_t));
+    for (size_t i = 0; i < new_count; i++) numbers[i] = rand();
+    printf("%zu %zu\n", numbers[count - 1], numbers[new_count - 1]);
+
+    {
+        size_t allocated = 0;
+        size_t allocated_len = sizeof(allocated);
+        mallctl("stats.allocated", &allocated, &allocated_len, NULL, 0);
+        printf("allocated: %zu\n", allocated);
+
+        size_t active = 0;
+        size_t active_len = sizeof(active);
+        mallctl("stats.active", &active, &active_len, NULL, 0);
+        printf("active: %zu\n", active);
+
+        size_t mapped = 0;
+        size_t mapped_len = sizeof(mapped);
+        mallctl("stats.mapped", &mapped, &mapped_len, NULL, 0);
+        printf("mapped: %zu\n", mapped);
+
+        size_t retained = 0;
+        size_t retained_len = sizeof(retained);
+        mallctl("stats.retained", &retained, &retained_len, NULL, 0);
+        printf("retained: %zu\n", retained);
+
+        size_t resident = 0;
+        size_t resident_len = sizeof(resident);
+        mallctl("stats.resident", &resident, &resident_len, NULL, 0);
+        printf("resident: %zu\n", resident);
+    }
 }
